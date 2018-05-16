b0e4bb33dd6de7a77a9bca229af470110c5a363c
Cantbedisable <Small-samoylov@yandex.ru>
2018-05-17 01:11:34 +0300

    Ya pravda staralsya v.2

--- YaPitalsya/js/JavaSCode.js
+++ YaPitalsya/js/JavaSCode.js
@@ -1,13 +1,36 @@
-var mass = new Array();
-function man (name,age){
-  this.name = name;
-  this.age = age;
-  mass.push(this);
+var tableContent = localStorage.getItem('tableContent');
+if(tableContent == null || tableContent == ""){
+  tableContent = [];
 }
-var Kir = new man("Кирилл",23);
-var Sanya = new man("Саша",42);
-for(var i = 0; i < mass.length; i++){
-  var name = mass[i].name;
-  var age = mass[i].age + "";
-  document.writeln("<i>" + name.fontcolor("blue") + "</i>" + " имеет возраст " + "<b>" + age.fontcolor("red") + "</b>" + "<br>");
+/*
+var VTB = new Bank(2132,"VTB",84564,"sosat");
+var ALFA = new Bank(2184,"ALFA",985,"hui");
+var SBER = new Bank(784,"SBER",369,"chlen");
+tableContent.push(VTB);
+tableContent.push(ALFA);
+tableContent.push(SBER);*/
+function contentTableOnReady(){
+  if(tableContent.length > 0){
+    var table = document.getElementById('bankTable');
+    var html = table.innerHTML.replace("<tbody>","").replace("</tbody>","");//небольшой костыль, по рукам сильно не бейте ток
+    for(var i = 0; i < tableContent.length; i++){
+      var bank = tableContent[i];
+      var bik = bank.BIK;
+      var name = bank.Name;
+      var corr = bank.corrAcc;
+      var adr = bank.adress;
+      var newTeg = "<tr>" + "<td>" + bik + "</td>"
+                          + "<td>" + name + "</td>"
+                          + "<td>" + corr + "</td>"
+                          + "<td>" + adr + "</td>"
+                  +"</tr>"
+      html += newTeg;
+    }
+    var newHtml = "<tbody>" + html + "</tbody>";
+    table.innerHTML = newHtml;
+  }
+}
+
+function onClickButtonAdd(){
+  document.location.href = "addForm.html";
 }
---
+++ YaPitalsya/js/jsCodeForAddForm.js
@@ -0,0 +1,36 @@
+var tableContent = localStorage.getItem('tableContent');
+if(tableContent == null || tableContent == ""){
+  tableContent = [];
+}
+
+function Bank(bik,name,corrAcc,adress){
+  this.BIK = bik;
+  this.Name = name;
+  this.corrAcc = corrAcc;
+  this.adress = adress;
+}
+
+function onClickBtnCancel(){
+  document.location.href = "index.html";
+}
+function onClickBtnSave(){debugger;
+  var bik = document.getElementById('BIK').value;
+  var name = document.getElementById('Name').value;
+  var corr = document.getElementById('corrAcc').value;
+  var adress = document.getElementById('adress').value;
+  var mess = "";
+  if(bik == ""){mess += 'Не заполнено поле "БИК"';}
+  if(name == ""){mess += 'Не заполнено поле "Наименование"';}
+  if(corr == ""){mess += 'Не заполнено поле "Корсчет"';}
+  if(adress == ""){adress += 'Не заполнено поле "Адрес"';}
+  //else if че то не алешка сегодня
+  if(mess == ""){
+    var newBank = new Bank(bik,name,corr,adress);
+    tableContent.push(newBank);
+    localStorage.setItem('tableContent',tableContent);
+    document.location.href = "index.html";
+  }else{
+    alert(mess);
+    return false;
+  }
+}
