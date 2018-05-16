1e4cbd54b054a97418517506ddb89faafa8e1620
Cantbedisable <Small-samoylov@yandex.ru>
2018-05-16 20:19:10 +0300

    Menya zovut Sanya, esli 4e ya bahau

--- YaPitalsya/JavaSCode.js
+++ YaPitalsya/JavaSCode.js
@@ -11,3 +11,27 @@ for(var i = 0; i < mass.length; i++){
   var age = mass[i].age + "";
   document.writeln("<i>" + name.fontcolor("blue") + "</i>" + " имеет возраст " + "<b>" + age.fontcolor("red") + "</b>" + "<br>");
 }
+
+
+function valid(form){debugger;
+  var mess = "";
+  var name = form.name.value;
+  var password = form.password.value;
+  var RePassword = form.RePassword.value;
+  var state = form.state.value;
+  if(name == "" || name == " " || name == "  ")
+    mess = mess + "Вы не ввели свое имя" + "\n";
+  if(password == "")
+    mess = mess + "Вы не ввели пароль"  + "\n";
+  if((RePassword != "") && (password != RePassword))
+    mess = mess + "Пароли не совпадают"  + "\n";
+  if(state == "")
+    mess = mess + "Укажите пол"  + "\n";
+
+  if(mess != ""){
+    alert(mess);
+  }else{
+    window.location = "index.html";
+    //window.reload  перезагрузка
+  }
+}
